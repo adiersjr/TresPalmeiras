37fad2bf0bbd4b3d09ba5b8f877267587fdcdcc4
japones <alexandre.homma@gmail.com>
2016-02-22 21:59:03 -0300

    a

--- src/br/com/xkinfo/model/Municipio.java
+++ src/br/com/xkinfo/model/Municipio.java
@@ -8,4 +8,34 @@ public class Municipio {
 	private String cep;
 	private String codigoIbge;
 	
+	public String getCep() {
+		return cep;
+	}
+	public String getCodigoIbge() {
+		return codigoIbge;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getSigla() {
+		return sigla;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public void setCodigoIbge(String codigoIbge) {
+		this.codigoIbge = codigoIbge;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
 }

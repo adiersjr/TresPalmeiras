8e37f92a909e24ff5b0dcce3df41c2fd56f1ff1b
japones <alexandre.homma@gmail.com>
2016-02-23 17:32:13 -0300

    a

---
+++ src/br/com/xkinfo/model/Setor.java
@@ -0,0 +1,85 @@
+package br.com.xkinfo.model;
+
+import java.util.Calendar;
+
+public class Setor {
+
+	private int id;
+	private String chave;
+	private Municipio municipio;
+	private String setor;
+	private String descricao;
+	private String ultimaFatura;
+	private Calendar dataUltimaFatura;
+	private Calendar dataUltimaExtFatura;
+	private String localizacao;
+	private String ultimaFaturaT;
+	private Calendar dataUltimaCalCat;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getChave() {
+		return chave;
+	}
+	public void setChave(String chave) {
+		this.chave = chave;
+	}
+	public Municipio getMunicipio() {
+		return municipio;
+	}
+	public void setMunicipio(Municipio municipio) {
+		this.municipio = municipio;
+	}
+	public String getSetor() {
+		return setor;
+	}
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getUltimaFatura() {
+		return ultimaFatura;
+	}
+	public void setUltimaFatura(String ultimaFatura) {
+		this.ultimaFatura = ultimaFatura;
+	}
+	public Calendar getDataUltimaFatura() {
+		return dataUltimaFatura;
+	}
+	public void setDataUltimaFatura(Calendar dataUltimaFatura) {
+		this.dataUltimaFatura = dataUltimaFatura;
+	}
+	public Calendar getDataUltimaExtFatura() {
+		return dataUltimaExtFatura;
+	}
+	public void setDataUltimaExtFatura(Calendar dataUltimaExtFatura) {
+		this.dataUltimaExtFatura = dataUltimaExtFatura;
+	}
+	public String getLocalizacao() {
+		return localizacao;
+	}
+	public void setLocalizacao(String localizacao) {
+		this.localizacao = localizacao;
+	}
+	public String getUltimaFaturaT() {
+		return ultimaFaturaT;
+	}
+	public void setUltimaFaturaT(String ultimaFaturaT) {
+		this.ultimaFaturaT = ultimaFaturaT;
+	}
+	public Calendar getDataUltimaCalCat() {
+		return dataUltimaCalCat;
+	}
+	public void setDataUltimaCalCat(Calendar dataUltimaCalCat) {
+		this.dataUltimaCalCat = dataUltimaCalCat;
+	}
+}
---
+++ src/br/com/xkinfo/model/Vencimento.java
@@ -0,0 +1,41 @@
+package br.com.xkinfo.model;
+
+public class Vencimento {
+
+	private int id;
+	private Setor setor;
+	private int numero;
+	private String dia;
+	private boolean vip;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Setor getSetor() {
+		return setor;
+	}
+	public void setSetor(Setor setor) {
+		this.setor = setor;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getDia() {
+		return dia;
+	}
+	public void setDia(String dia) {
+		this.dia = dia;
+	}
+	public boolean isVip() {
+		return vip;
+	}
+	public void setVip(boolean vip) {
+		this.vip = vip;
+	}
+}

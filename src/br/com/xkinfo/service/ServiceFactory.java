42f2de21bf4f2883ea1669abee7d6d9a8cb925a3
japones <alexandre.homma@gmail.com>
2016-02-27 14:25:46 -0300

    a

--- src/br/com/xkinfo/service/ServiceFactory.java
+++ src/br/com/xkinfo/service/ServiceFactory.java
@@ -1,13 +1,18 @@
 package br.com.xkinfo.service;
 
 import br.com.xkinfo.service.impl.CargoService;
+import br.com.xkinfo.service.impl.FuncionarioService;
 
 public class ServiceFactory {
 
 	public static final ICargoService cargoService = new CargoService();
+	public static final IFuncionarioService funcionarioService = new FuncionarioService();
 	
 	
 	public static ICargoService getCargoservice() {
 		return cargoService;
+	}
+	public static IFuncionarioService getFuncionarioservice() {
+		return funcionarioService;
 	}
 }

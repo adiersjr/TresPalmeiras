fa08f0dfb6a71ead9ce8df0fe87fc67b5a1e006a
adiersjr <adiersjr@gmail.com>
2016-03-03 21:55:12 -0300

    TESTE

--- src/br/com/xkinfo/model/Empresa.java
+++ src/br/com/xkinfo/model/Empresa.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package br.com.xkinfo.model;
 
 import java.util.Calendar;
@@ -156,3 +157,32 @@ public class Empresa {
 		this.logo = logo;
 	}
 }	
+=======
+package br.com.xkinfo.model;
+
+import java.util.Calendar;
+
+public class Empresa {
+
+	private int id;
+	private String razaoSocial;
+	private String nomeFantasia;
+	private Calendar dataImplantacao;
+	private String inscricaoEstadual;
+	private String cnpj;
+	private String numeroJuntaComercial;
+	private Calendar dataJuntaComercial;
+	private String telefone;
+	private String rua;
+	private String numeroImovel;
+	private String bairro;
+	private Estado estado;
+	private String municipio;
+	private String responsavel;
+	private String cargoResponsavel;
+	private String cpfResponsavel;
+	private String contador;
+	private String cpfContador;
+	private String logo;  // no banco grava blob
+}	
+>>>>>>> Stashed changes

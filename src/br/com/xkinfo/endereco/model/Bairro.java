6c6f66c90fe69ab84b838cd16f564ba575565b5e
japones <alexandre.homma@gmail.com>
2016-03-08 15:56:37 -0300

    A

--- src/br/com/xkinfo/endereco/model/Bairro.java
+++ src/br/com/xkinfo/endereco/model/Bairro.java
@@ -4,22 +4,15 @@ public class Bairro {
 
 	private int id;
 	private String nome;
-	private Municipio municipio;
 	
 	public int getId() {
 		return id;
-	}
-	public Municipio getMunicipio() {
-		return municipio;
 	}
 	public String getNome() {
 		return nome;
 	}
 	public void setId(int id) {
 		this.id = id;
-	}
-	public void setMunicipio(Municipio municipio) {
-		this.municipio = municipio;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
